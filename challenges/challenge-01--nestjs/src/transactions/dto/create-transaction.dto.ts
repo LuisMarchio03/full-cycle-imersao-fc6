3531a2d9af172cec3cc3e1aa2e8b8c09bb5cfaba
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2022-02-12 12:05:55 -0300

    Delete challenges directory

--- challenges/challenge-01--nestjs/src/transactions/dto/create-transaction.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateTransactionDto {
-  account_id: number;
-  amount: number;
-}
--- challenges/challenge-01--nestjs/src/transactions/repositories/transactions.repository.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from 'typeorm';
-import { Transaction } from './entities/transaction.entity';
-
-export class TransactionsRepository extends Repository<Transaction> {}
